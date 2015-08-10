626fb9c77371c9c69a20d1fe59ac1a93fd787274
Guanaco77 <debruecker@gmail.com>
2015-08-10 12:32:46 +0200

    change

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://medivisu.com'
+SITEURL = 'http://medivisu.github.io/Website/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
